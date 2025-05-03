50e750ee120f8125a0358e3134e4609a86b5e788
Aneiv <161586104+Aneiv@users.noreply.github.com>
2025-05-03 16:28:44 +0200

    Add money amount info in ingameUI

--- core/src/main/java/com/badlogic/drop/InGameUI.java
+++ core/src/main/java/com/badlogic/drop/InGameUI.java
@@ -17,19 +17,27 @@ public class InGameUI {
     private final Stage stage;
     private float upperUI_width = 50f;
     private final String exitButton_Text = "EXIT";
-    private Label scoreAmountTest;
+    private Label scoreAmountText;
+    private Label moneyAmountText;
     //umieszczenie tekstu score
     float labelWidth = 200f; // szerokość pola tekstowego na gameUI (gorna czesc ekranu
     float scoreAmountTextPosX = Gdx.graphics.getWidth() - labelWidth - 20f; //20px margines od prawej - dla wyniku score
     float scoreTextPosX = Gdx.graphics.getWidth() - 500f; //X dla tekstu Score
     float scoreTextPosY = Gdx.graphics.getHeight() - upperUI_width - 12;//Y dla tekstu Score
+
+    //umieszczenie money amount text
+    float MoneyAmount_offset = 300f;
+
     public InGameUI(final Main game){
         this.game = game;
         shapeRenderer = new ShapeRenderer();
         batch = new SpriteBatch();
         stage = new Stage(new ScreenViewport());
-        scoreAmountTest = TextFieldFactory.create(
+        scoreAmountText = TextFieldFactory.create(
             "0", UsefullConstans.textSize, scoreAmountTextPosX, scoreTextPosY, Color.WHITE
+        );
+        moneyAmountText = TextFieldFactory.create(
+            "0", UsefullConstans.textSize, MoneyAmount_offset, scoreTextPosY, Color.WHITE
         );
 
     }
@@ -42,13 +50,23 @@ public class InGameUI {
         Label scoreText = TextFieldFactory.create(
             "SCORE:", UsefullConstans.textSize, scoreTextPosX, scoreTextPosY, Color.WHITE
         );
+        //Money napis
+        Label MoneyText = TextFieldFactory.create(
+            "MONEY:", UsefullConstans.textSize,  20f, scoreTextPosY, Color.WHITE
+        );
         //Score - liczba
         // ustawiamy szerokość i justowanie w Label
-        scoreAmountTest.setSize(labelWidth, UsefullConstans.textSize + 10);
-        scoreAmountTest.setAlignment(Align.right);//od prawej strony ekranu align
+        scoreAmountText.setSize(labelWidth, UsefullConstans.textSize + 10);
+        scoreAmountText.setAlignment(Align.right);//od prawej strony ekranu align
+
+        //Money - liczba
+        // ustawiamy szerokość i justowanie w Label
+        scoreAmountText.setSize(labelWidth, UsefullConstans.textSize + 10);
 
         stage.addActor(scoreText);
-        stage.addActor(scoreAmountTest);
+        stage.addActor(MoneyText);
+        stage.addActor(scoreAmountText);
+        stage.addActor(moneyAmountText);
     }
     public void render() {
         // Tło górnego paska UI
@@ -56,7 +74,9 @@ public class InGameUI {
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 
         //aktualizacja wyniku score do rysowania
-        scoreAmountTest.setText(game.score);
+        scoreAmountText.setText(game.score);
+        //aktualizacja money amount do rysowania
+        moneyAmountText.setText(game.money);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(0, 0, 0, 1f); // Czarne tło
         shapeRenderer.rect(0, Gdx.graphics.getHeight() - upperUI_width, Gdx.graphics.getWidth(), upperUI_width);
